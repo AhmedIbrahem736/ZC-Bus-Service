18453fc3367b9f0fe6b89fd998d16b6154945050
Ahmed Adel <69484554+Ahmed23Adel@users.noreply.github.com>
2024-05-10 20:16:24 +0300

    Created Bus stop model and all its CRUD operations

--- apps/bus/models.py
+++ apps/bus/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from apps.users.models import User
 from apps.base.models import CustomBaseModel
+from django.utils.translation import gettext_lazy as _
 
 class BusRoute(CustomBaseModel):
     destination = models.CharField(max_length=100, unique=True, blank=False)
@@ -11,3 +12,15 @@ class BusRoute(CustomBaseModel):
 
     def __str__(self):
         return self.destination
+    
+
+class BusStop(CustomBaseModel):
+    name = models.CharField(max_length=100, blank=False)
+    address = models.CharField(max_length=255, blank=False)
+    map_location = models.CharField(max_length=255)
+    departure_time = models.TimeField(_("Departure Time"))
+    arrival_time = models.TimeField(_("Arrival Time"))
+    bus_route = models.ForeignKey(BusRoute, on_delete=models.CASCADE, related_name='bus_stops')
+
+    def __str__(self):
+        return self.name
